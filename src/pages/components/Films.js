aadf8f122023cc4a58a382e6b044c4feb0f4648b
curtis clayton <curtisclayton4270@gmail.com>
2023-04-18 11:17:07 -0400

    Added light ad dark theme toggle

--- src/pages/components/Films.js
+++ src/pages/components/Films.js
@@ -16,84 +16,49 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 
-{
-  // {title: 'Revenge of the Sith', episode_id: 3, opening_crawl: 'War! The Republic is crumbling\r\nunder attacks by t…ate mission to rescue the\r\ncaptive Chancellor....', director: 'George Lucas', producer: 'Rick McCallum', …}
-  // characters : (34) ['https://swapi.dev/api/people/1/', 'https://swapi.dev/api/people/2/', 'https://swapi.dev/api/people/3/', 'https://swapi.dev/api/people/4/', 'https://swapi.dev/api/people/5/', 'https://swapi.dev/api/people/6/', 'https://swapi.dev/api/people/7/', 'https://swapi.dev/api/people/10/', 'https://swapi.dev/api/people/11/', ...']
-  // created: "2014-12-20T18:49:38.403000Z"
-  // director : "George Lucas"
-  // edited : "2014-12-20T20:47:52.073000Z"
-  // episode_id : 3
-  // opening_crawl : "War! The Republic is crumbling\r\nunder attacks by the ruthless\r\nSith Lord, Count Dooku.\r\nThere are heroes on both sides.\r\nEvil is everywhere.\r\n\r\nIn a stunning move, the\r\nfiendish droid leader, General\r\nGrievous, has swept into the\r\nRepublic capital and kidnapped\r\nChancellor Palpatine, leader of\r\nthe Galactic Senate.\r\n\r\nAs the Separatist Droid Army\r\nattempts to flee the besieged\r\ncapital with their valuable\r\nhostage, two Jedi Knights lead a\r\ndesperate mission to rescue the\r\ncaptive Chancellor...."
-  // planets : (13) ['https://swapi.dev/api/planets/1/', 'https://swapi.dev/api/planets/2/', 'https://swapi.dev/api/planets/5/', 'https://swapi.dev/api/planets/8/', 'https://swapi.dev/api/planets/9/', 'https://swapi.dev/api/planets/12/', ...]
-  // producer: "Rick McCallum"
-  // release_date :"2005-05-19"
-  // species: (20) ['https://swapi.dev/api/species/1/', 'https://swapi.dev/api/species/2/', 'https://swapi.dev/api/species/3/', 'https://swapi.dev/api/species/6/', 'https://swapi.dev/api/species/15/', 'https://swapi.dev/api/species/19/',...']
-  // starships : (12) ['https://swapi.dev/api/starships/2/', 'https://swapi.dev/api/starships/32/', 'https://swapi.dev/api/starships/48/', 'https://swapi.dev/api/starships/59/', ...]
-  // title : "Revenge of the Sith"
-  // url : "https://swapi.dev/api/films/6/"
-  // vehicles : (13) ['https://swapi.dev/api/vehicles/33/', 'https://swapi.dev/api/vehicles/50/', 'https://swapi.dev/api/vehicles/53/', 'https://swapi.dev/api/vehicles/56/',...]
-  // [[Prototype]] : Object
-}
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  useColorScheme,
+} from '@mui/material/styles';
 
 // create Image array fot the posters
 let posters = [
   {
-    episode_id: 1,
+    id: 1,
     image: "/films/thePhantomMenace.jpg",
   },
   {
-    episode_id: 2,
+    id: 2,
     image: "/films/attackOfTheClones.jpg",
-  }, 
+  },
   {
-    episode_id: 3,
+    id: 3,
     image: "/films/revengeOfTheSith.jpg",
   },
   {
-    episode_id: 4,
+    id: 4,
     image: "/films/aNewHope.jpg",
   },
   {
-    episode_id: 5,
+    id: 5,
     image: "/films/empireStrikesBack.jpg",
-  }, 
+  },
   {
-    episode_id: 6,
+    id: 6,
     image: "/films/returnOfTheJedi.jpg",
   },
 ];
 
-function IsFilm() {
-  let filterFilms = [];
-
-  for(let i = 0; posters.length > i; i++) {
-    console.log("🚀 ~ file: Films.js:70 ~ IsFilm ~ posters:", posters)
-    
-  }
-
-}
-
-
-
 const Films = () => {
   const [films, setFilms] = useState([]);
   const [open, setOpen] = useState(true);
-
-
-  
-
-  // useEffect((episode_id) => {
-  // if(episode_id === posters.episode_id){
-
-  // }
-  // }, [])
-
-
+  const [image, setImage] = useState("");
 
   const handleClick = () => {
     setOpen(!open);
   };
 
+  // Fetch films
   useEffect(() => {
     getAllData("https://swapi.dev/api/films/").then((data) => {
       if (data.results) {
@@ -104,13 +69,62 @@ const Films = () => {
     });
   }, []);
 
+  //  if posters.id matches film show poster
+  useEffect(() => {
+
+    films.map((data) => {
+      posters.map((poster) => {
+        let currentFilmId =  data.episode_id;
+        let currentPosterId = poster.id;   
+        let posterImage = poster.image;
+
+        if(currentFilmId === currentPosterId) {
+          setImage(posterImage)
+        }
+           
+      });    
+    });
+  }, [films]);
+
+  const renderPoster = (episode_id) => {
+  const poster =  posters.filter((x) => episode_id === x.id);
+  return poster[0].image;
+
+  };
+
+
+
+  // const Item = styled(Paper)(({ theme }) => ({
+  //   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  //   ...theme.typography.body2,
+  //   padding: theme.spacing(1),
+  //   textAlign: "center",
+  //   color: theme.palette.text.secondary,
+  //   margin: "10px 0",
+  //   width: "100%",
+  // }));
+
+  const StyledH5 = styled(Typography)(({theme }) => ({
+    ...theme.typography.h4,
+    // fontFamily: "Star Wars",
+  }));
+
+  const StyledBody = styled(Typography)(({theme }) => ({
+    ...theme.typography.body1,
+      // minHeight: "390px",
+      border: "1px dotted black",
+  }));
+
+  const StyledScrollText = styled(Typography)(({theme }) => ({
+    ...theme.typography.body2,
+      border: "1px solid red",
+  }));
+
+
   return (
     <>
       {films.map((film) => {
-        {/* console.log("🚀 ~ file: GetFilms.js:36 ~ {film.filter ~ film", film); */}
-        {
-          /* reuseable below just change for the api data ie. film, characters */
-        }
+        { /* reuseable below just change for the api data ie. film, characters */ }
         const {
           title,
           created,
@@ -125,8 +139,9 @@ const Films = () => {
           vehicles,
         } = film;
 
-
-        {  /* I want to set the image for each Films movie to the poster */}
+        {
+          /* I want to set the image for each Films movie to the poster */
+        }
 
         return (
           <Grid
@@ -138,33 +153,39 @@ const Films = () => {
               mx: "10px",
               my: "20px",
               width: "465px",
-              border: "1px dotted yellow",
-                
+
+              // border: "1px dotted yellow",
             }}
           >
             <Grid item>
-            <IsFilm />
-              <Card 
-              // sx={{ maxWidth: "365px", }}
+              <Card
+              sx={{ maxWidth: "420px",
+               maxHeight: "865px",
+                border: "2px solid blue",
+                 }}
               >
-                <CardMedia
-                  component="img"
-                  alt=""
-                  height="140"
-                  image="/films/revengeOfTheSith.jpg"
-                />
+                <CardMedia 
+                component="img" 
+                alt="movie poster" 
+                height="140" 
+                image={renderPoster(episode_id)}
+                 />
                 <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
+                  <StyledH5>
                     {title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary" height="325px">
-                    {opening_crawl}
-                  </Typography>
-                </CardContent>
+                  </StyledH5>
                 <CardActions>
                   <Button size="small">Share</Button>
                   <Button size="small">Learn More</Button>
                 </CardActions>
+                 <StyledBody>
+                 <StyledScrollText>
+
+                  {opening_crawl}
+                 </StyledScrollText>
+                 </StyledBody>
+
+                </CardContent>
               </Card>
             </Grid>
           </Grid>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useState, useEffect } from "react";
 import Films from "./components/Films";
+import ThemeToggle from "./components/ThemeToggle";
 
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  useColorScheme,
+} from '@mui/material/styles';
+
+// MUI Imports
 import Box from "@mui/material/Box";
-
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 
 import getAllData from "./api/getAllData";
@@ -13,24 +18,32 @@ import getAllData from "./api/getAllData";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ film }) {
-  const [people, setPeople] = useState([]);
-
-  useEffect(() => {
-    getAllData("https://swapi.dev/api/people").then((data) => {
-      setPeople(data.results);
-    });
-  }, []);
+  // add toggle light and dark with star wars icons and matching music
 
   return (
-    <>
+    <Box>
+    <CssVarsProvider>
       <h1>Star Wars pre-Work</h1>
-      <Box sx={{ 
-        display: "grid",
-                gridTemplateColumns: "1fr 1fr 1fr",
-        //  flexWrap: "wrap"
-          }}>
-        <Films />
-      </Box>
-    </>
+      <Grid container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+      >
+      <ThemeToggle />
+        <Grid
+          item
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "0.2fr 0.2fr 0.2fr",
+            border: "1px solid green",
+          }}
+        >
+          <Films />
+        </Grid>
+      </Grid>
+      </CssVarsProvider>
+    </Box>
+
   );
 }
